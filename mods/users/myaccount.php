f828e6c0ad0f85ad2b1f3120364e091480c5dccb
Mike Amundsen <mamundsen@coronapro.com>
2011-06-10 16:58:49 -0400

    MyAccount page

---
+++ mods/users/myaccount.php
@@ -0,0 +1,6 @@
+<table border="0" cellspacing="0" cellpadding="0" class="userlist-form">
+<tr><td align="right" class="ftitle">UserName:</td><td class="ffield"><?php echo $userinfo->usr_name; ?></td></tr>
+<tr><td align="right" class="ftitle">Full Name:</td><td class="ffield"><?php echo $userinfo->usr_fullname; ?></td></tr>
+<tr><td align="right" class="ftitle">EMail:</td><td class="ffield"><?php echo $userinfo->usr_email; ?></td></tr>
+</table>
+<?php
